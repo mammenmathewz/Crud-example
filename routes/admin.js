2512c193fa079d46613529ce68796f190e5dfebc
Mammen Mathew <mammen999@gmail.com>
2023-11-15 19:11:07 +0530

    basic completed

--- routes/admin.js
+++ routes/admin.js
@@ -9,9 +9,9 @@ app.use(express.json())
 
 router.get('/', (req, res) => {
   if (req.session.user) {
-     return res.render('./admin/adminhome');
+     return res.render('adminhome');
   }
-  res.render('./admin/adminlogin')
+  res.render('adminlogin')
 });
 
 
@@ -23,10 +23,10 @@ router.get('/', (req, res) => {
  router.post('/adminhome', (req, res) => {
    if (req.body.email === "admin@gmail.com" && req.body.password === "12345") {
     req.session.user=req.body.email
-    res.render('./admin/adminhome');
+    res.render('adminhome');
      console.log(req.body);
    } else {
-     res.redirect('/admin/adminlogin');
+     res.redirect('adminlogin');
    }
  });
  
@@ -37,7 +37,7 @@ router.get('/', (req, res) => {
       console.log(err);
       res.render('error',{title: "404"})
     } else {
-      res.render('admin/adminlogin')
+      res.render('adminlogin')
     }
   })
 })
@@ -46,7 +46,7 @@ router.get('/', (req, res) => {
 
 
 router.get('/newuser',(req,res)=>{
-    res.render('./admin/adduser')
+    res.render('adduser')
  })
 
 module.exports=router
