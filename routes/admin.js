60207c5dc62db591a73fd42bac6ee985eb36a62c
Mammen Mathew <mammen999@gmail.com>
2023-11-15 23:56:38 +0530

    delete and update is commented

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const bodyParser = require('body-parser');
+const User = require('../models/users');
+const session = require('express-session');
 const app = express();
 
 
@@ -14,22 +16,44 @@ router.get('/', (req, res) => {
   res.render('adminlogin')
 });
 
+router.get('/adminhome', async (req, res) => {
+  if (req.session.user) {
+    try {
+      let users = await User.find({}); // Fetch all users from the database
+      if (!users) users = []; // If users is undefined, set it to an empty array
+      res.render('adminhome', { users: users }); // Pass the users to the view
+    } catch (err) {
+      console.error(err);
+      res.send('Error occurred while fetching data');
+    }
+  }else{
+    res.redirect('/admin');
+  }
+  });
+
+// router.get('/adminhome', (req, res) => {
+//   if (req.session.user) {
+//      return res.render('adminhome');
+//   }
+//   res.redirect('/admin');
+// });
 
 
 // router.get('/home',(req, res) => {
 //     res.render('./admin/adminhome')
 //  }); 
 
- router.post('/adminhome', (req, res) => {
-   if (req.body.email === "admin@gmail.com" && req.body.password === "12345") {
-    req.session.user=req.body.email
-    res.render('adminhome');
-     console.log(req.body);
-   } else {
-     res.redirect('adminlogin');
-   }
- });
- 
+router.post('/adminhome', async (req, res) => {
+  if (req.body.email === "admin@gmail.com" && req.body.password === "12345") {
+   req.session.user=req.body.email
+   let users = await User.find({});
+   res.render('adminhome', { users: users });
+    console.log(req.body);
+  } else {
+    res.redirect('adminlogin');
+  }
+});
+
 
  router.get('/logoutadmin', (req, res) => {
   req.session.destroy(function (err) {
@@ -41,12 +65,78 @@ router.get('/', (req, res) => {
     }
   })
 })
- 
+
 
 
 
 router.get('/newuser',(req,res)=>{
-    res.render('adduser')
+    res.render('adduser',)
  })
+
+ router.post('/add', async (req, res) => {
+  const newUser = new User({ // Changed 'user' to 'User'
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone,
+      password: req.body.password
+  });
+  try {
+      await newUser.save();
+      req.session.message = 'User successfully added';
+      res.redirect('adminhome');
+  } catch (err) {
+      res.json({message: err.message, type: 'danger'});
+  }
+});
+
+
+//update//
+
+// router.get('/admin/edit/:id', async (req, res) => {
+//   try {
+//     let user = await User.findById(req.params.id);
+//     if (!user) {
+//       return res.status(404).send('User not found');
+//     }
+//     res.render('edituser', { user: user });
+//   } catch (err) {
+//     console.error(err);
+//     res.send('Error occurred while fetching user data');
+//   }
+// });
+
+
+// router.post('admin/edit/:id', async (req, res) => {
+//   try {
+//     let user = await User.findById(req.params.id);
+//     if (!user) {
+//       return res.status(404).send('User not found');
+//     }
+//     user.name = req.body.name;
+//     user.email = req.body.email;
+//     user.phone = req.body.phone;
+//     user.password = req.body.password;
+
+//     await user.save();
+//     res.redirect('/adminhome');
+//   } catch (err) {
+//     console.error(err);
+//     res.send('Error occurred while updating user data');
+//   }
+// });
+
+
+// router.get('/admin/delete/:id', async (req, res) => {
+//   try {
+//     let user = await User.findByIdAndRemove(req.params.id);
+//     if (!user) {
+//       return res.status(404).send('User not found');
+//     }
+//     res.redirect('/adminhome');
+//   } catch (err) {
+//     console.error(err);
+//     res.send('Error occurred while deleting user');
+//   }
+// });
 
 module.exports=router
